fcc51f4231c22586cbf7667d5808cb9469c16f87
Alan Somers <asomers@gmail.com>
2023-12-07 11:08:16 -0700

    Add some negative test cases

--- tests/functional/nix.rs
+++ tests/functional/nix.rs
@@ -4,23 +4,41 @@ use std::{
 };
 
 use capsicum_net::CasperExt;
-use nix::sys::socket::{
-    AddressFamily, SockaddrIn, SockFlag, SockType, getsockname, socket
+use nix::{
+    Error,
+    sys::socket::{AddressFamily, SockaddrIn, SockFlag, SockType, getsockname,
+        socket}
 };
 
 use crate::CASPER;
 
-#[test]
-fn nix() {
-    let mut casper = CASPER.get().unwrap().lock().unwrap();
-    let mut cap_net = casper.net().unwrap();
+mod bind {
+    use super::*;
 
-    let s = socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(), None)
-        .unwrap();
-    let want = SockaddrIn::from_str("127.0.0.1:8082").unwrap();
-    cap_net.bind(s.as_raw_fd(), &want).unwrap(); 
-    let bound: SockaddrIn = getsockname(s.as_raw_fd()).unwrap();
-    assert_eq!(want, bound);
+    #[test]
+    fn eafnosupport() {
+        let mut casper = CASPER.get().unwrap().lock().unwrap();
+        let mut cap_net = casper.net().unwrap();
+
+        let s = socket(AddressFamily::Inet6, SockType::Stream,
+                       SockFlag::empty(), None)
+            .unwrap();
+        let want = SockaddrIn::from_str("127.0.0.1:8082").unwrap();
+        let err = cap_net.bind(s.as_raw_fd(), &want).unwrap_err(); 
+        assert_eq!(err, Error::EAFNOSUPPORT);
+    }
+
+    #[test]
+    fn ok() {
+        let mut casper = CASPER.get().unwrap().lock().unwrap();
+        let mut cap_net = casper.net().unwrap();
+
+        let s = socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(),
+             None)
+            .unwrap();
+        let want = SockaddrIn::from_str("127.0.0.1:8082").unwrap();
+        cap_net.bind(s.as_raw_fd(), &want).unwrap(); 
+        let bound: SockaddrIn = getsockname(s.as_raw_fd()).unwrap();
+        assert_eq!(want, bound);
+    }
 }
-
-
--- tests/functional/tokio.rs
+++ tests/functional/tokio.rs
@@ -2,17 +2,31 @@ use capsicum_net::CasperExt;
 
 use crate::CASPER;
 
-#[tokio::test]
-async fn tokio() {
-    use capsicum_net::tokio::TcpSocketExt;
-    let mut casper = CASPER.get().unwrap().lock().unwrap();
-    let mut cap_net = casper.net().unwrap();
+mod bind {
+    use super::*;
 
-    let want = "127.0.0.1:8083".parse().unwrap();
-    let socket = tokio::net::TcpSocket::new_v4().unwrap();
-    socket.cap_bind(&mut cap_net, want).unwrap();
-    let bound = socket.local_addr().unwrap();
-    assert_eq!(want, bound);
+    #[tokio::test]
+    async fn eafnosupport() {
+        use capsicum_net::tokio::TcpSocketExt;
+        let mut casper = CASPER.get().unwrap().lock().unwrap();
+        let mut cap_net = casper.net().unwrap();
+
+        let want = "127.0.0.1:8083".parse().unwrap();
+        let socket = tokio::net::TcpSocket::new_v6().unwrap();
+        let err = socket.cap_bind(&mut cap_net, want).unwrap_err();
+        assert_eq!(err.raw_os_error(), Some(libc::EAFNOSUPPORT));
+    }
+
+    #[tokio::test]
+    async fn ok() {
+        use capsicum_net::tokio::TcpSocketExt;
+        let mut casper = CASPER.get().unwrap().lock().unwrap();
+        let mut cap_net = casper.net().unwrap();
+
+        let want = "127.0.0.1:8083".parse().unwrap();
+        let socket = tokio::net::TcpSocket::new_v4().unwrap();
+        socket.cap_bind(&mut cap_net, want).unwrap();
+        let bound = socket.local_addr().unwrap();
+        assert_eq!(want, bound);
+    }
 }
-
-
